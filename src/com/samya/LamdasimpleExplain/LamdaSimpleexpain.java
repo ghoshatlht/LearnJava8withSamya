22a6db0e29e00519611bf3028b8dac0714f447a9
SG05477 <SG05477@denb0271.de.corp.danet.com>
2017-07-26 15:14:59 +0200

    Lamda

---
+++ src/com/samya/LamdasimpleExplain/LamdaSimpleexpain.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package com.samya.LamdasimpleExplain;
+
+/**
+ * @author Samya Ghosh
+ *
+ */
+
+
+interface A
+{
+	void show();
+}
+
+
+/*class B implements A
+{
+
+	@Override
+	public void show() {
+		System.out.println("hello from samya");
+		
+	}
+	
+}
+*/
+
+public class LamdaSimpleexpain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		A obj;
+		obj=() ->
+		{
+		
+			System.out.println("hello from samya");
+		
+		};
+		
+		
+		obj.show();
+		
+	}
+
+}
